e4f6bf6c96bf2d67e5f5f596f3ac8ad4173958de
Ayshe Izetova <stu2301681012@uni-plovdiv.bg>
2025-10-30 19:03:50 +0200

    Add Invoice model and update Program.cs

--- ST2-2025-Team5-InvoiceApp/Program.cs
+++ ST2-2025-Team5-InvoiceApp/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ST2_2025_Team5_InvoiceApp.Services.AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
---
+++ ST2-2025-Team5-InvoiceApp/Services/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ST2_2025_Team5_InvoiceApp.Services
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
