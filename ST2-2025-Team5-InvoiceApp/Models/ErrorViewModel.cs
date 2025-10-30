c209606b306050363abb41b54d81f4d299bff9dc
Ayshe Izetova <stu2301681012@uni-plovdiv.bg>
2025-10-30 18:53:52 +0200

    Initial commit - MVC project with database connection

---
+++ ST2-2025-Team5-InvoiceApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST2_2025_Team5_InvoiceApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
