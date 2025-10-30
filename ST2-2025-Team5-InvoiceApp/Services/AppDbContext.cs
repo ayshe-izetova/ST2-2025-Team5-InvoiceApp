8359ff1c9df606e0dd6dea4513e05b394db6a205
Ayshe Izetova <stu2301681012@uni-plovdiv.bg>
2025-10-30 19:12:19 +0200

    Added Invoice model with sample data

--- ST2-2025-Team5-InvoiceApp/Services/AppDbContext.cs
+++ ST2-2025-Team5-InvoiceApp/Services/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ST2_2025_Team5_InvoiceApp.Models;
 
 namespace ST2_2025_Team5_InvoiceApp.Services
 {
@@ -7,5 +8,7 @@ namespace ST2_2025_Team5_InvoiceApp.Services
         public AppDbContext(DbContextOptions options) : base(options)
         {
         }
+
+        public DbSet<Invoice> Invoices { get; set; } = null!;
     }
 }
