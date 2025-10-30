0c7c8e34dacbcba7b15587c76276fba893ba2795
Ayshe Izetova <stu2301681012@uni-plovdiv.bg>
2025-10-30 21:33:38 +0200

    Add InvoiceDto

---
+++ ST2-2025-Team5-InvoiceApp/Models/InvoiceDto.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ST2_2025_Team5_InvoiceApp.Models
+{
+    public class InvoiceDto
+    {
+        [Required]
+        public string Number { get; set; } = "";
+        [Required]
+        public string Status { get; set; } = "";
+        public DateOnly? IssueDate { get; set; }
+        public DateOnly? DueDate { get; set; }
+
+        //service details
+        [Required]
+        public string Service { get; set; } = "";
+        [Range(1,999999, ErrorMessage="Unit Price is not valid!")]
+        public decimal UnitPrice { get; set; }
+        [Range (1,99)]
+        public int Quantity { get; set; }
+
+        //client details
+        [Required(ErrorMessage ="Client Name is required")]
+        public string ClientName { get; set; } = "";
+        [Required, EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; } = "";
+        [Phone]
+        public string Phone { get; set; } = "";
+        public string Address { get; set; } = "";
+    }
+}
